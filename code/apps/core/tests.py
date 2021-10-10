c385b399e267d74c5348eec10c5aafa0078b6da2
Oleg Smedyuk <conformist.mw@gmail.com>
2021-10-11 00:10:26 +0300

    use core views as test

--- code/apps/core/tests.py
+++ code/apps/core/tests.py
@@ -1,5 +1,9 @@
+from django.shortcuts import reverse
+
+
 def test_index(client):
-    response = client.get('/')
+    url = reverse('core:test_index')
+    response = client.get(url)
     assert response.status_code == 200
     assert 'core/index.html' in [
         template.name for template in response.templates
